b655edcbc8fa41a1b7db6902669993740e494ef3
Mario Shtika <mario@tessera.gr>
2014-06-17 17:17:48 +0300

    fixed small bugs

--- burstiness/burstiness.php
+++ burstiness/burstiness.php
@@ -9,16 +9,33 @@ $db = $m->twitter_db;
 // select a collection (analogous to a relational database's table)
 $collection = $db->TwitterDigger;
 
-echo "Prepare Burstiness table: <strong>ok</strong><br />";
-
-echo '<hr />';
-
 // select a collection (analogous to a relational database's table)
 $burstinessCollection = $db->BurstinessCollection;
-
-// clean table (remove all documents)
-$burstinessCollection->remove();
 		
+if(isset($_GET['func'])) {
+	if($_GET['func'] == 'run') {
+		echo "Prepare Burstiness table: <strong>ok</strong><br />";
+
+		echo '<hr />';
+		
+		// clean table (remove all documents)
+		$burstinessCollection->remove();
+		
+		// find everything in english from the collection
+		$documents = $collection->find(array('lang'=>'en', 'text' => new MongoRegex('/'.$_GET['keyword'].'/')));
+
+		// iterate through the results
+		foreach ($documents as $tweet) {
+			$burstinessCollection->insert(array(
+				'time' => strtotime($tweet['created_at']),
+			));
+		}
+	}
+} else {
+	echo '<a class="btn btn-success" href="index.php?route=burstiness&keyword='.$_GET['keyword'].'&func=run" role="button">Extract Burstiness</a>';
+	echo '<hr />';
+}
+
 // count rows in the collection
 $allTweets = $collection->count();
 echo 'All tweets: <strong>'.$allTweets.'</strong><br />';
@@ -28,24 +45,30 @@ $tweetsInEnglish = $collection->count(array('lang'=>'en'));
 echo 'Tweets in english: <strong>'.$tweetsInEnglish.'</strong><br />';
 
 // count rows in the collection
-
 $keywordTweets = $collection->count(array('lang'=>'en', 'text' => new MongoRegex('/'.$_GET['keyword'].'/')));
 echo 'All tweets with the keyword '.$_GET['keyword'].': <strong>'.$keywordTweets.'</strong><br />';
 
-// find everything in english from the collection
-$documents = $collection->find(array('lang'=>'en', 'text' => new MongoRegex('/'.$_GET['keyword'].'/')));
-
-// iterate through the results
-foreach ($documents as $tweet) {
-	$burstinessCollection->insert(array(
-		'time' => strtotime($tweet['created_at']),
-		'tweet' => $tweet['text']
-	));
-}
-	
-
-echo '<hr />';
-
 echo 'Show chart: Burstines of <strong>'.$_GET['keyword'].'</strong>';
+
+// create array with time and burstiness
+$terms = $burstinessCollection->find();
+$terms = $terms->sort(array('time' => 1));
+
+$time = 0;
+$timeFrequency = 0;
+
+foreach ($terms as $term) {
+	//echo 'term time: '.$term['time'].'<br />';
+	//echo 'time: '.$time.'<br /><br />';
+	//echo 'frequency: '.$timeFrequency.'<br /><br />';
+	if($time != $term['time']) {
+		echo $term['time'].': '.$timeFrequency.' times<br />';
+		$time = $term['time'];
+		$timeFrequency = 1;
+	} else {
+		$timeFrequency++;
+	}
+	//echo $term['time'].'<br />';
+}
 
 ?>
--- term-frequency/index.php
+++ term-frequency/index.php
@@ -16,16 +16,72 @@ $db = $m->twitter_db;
 // select a collection (analogous to a relational database's table)
 $collection = $db->TwitterDigger;
 
-echo "Prepare Term Frequency table: <strong>ok</strong><br />";
-
-echo '<hr />';
-
 // select a collection (analogous to a relational database's table)
 $termCollection = $db->TermCollection;
 
-// clean table (remove all documents)
-$termCollection->remove();
+if(isset($_GET['func'])) {
+	if($_GET['func'] == 'run') {
+		echo "Prepare Term Frequency table: <strong>ok</strong><br />";
+
+		echo '<hr />';
+
+		// clean table (remove all documents)
+		$termCollection->remove();
 		
+		// find everything in english from the collection
+		$documents = $collection->find(array('lang'=>'en'));
+
+		// iterate through the results
+		foreach ($documents as $tweet) {
+			// 1st process
+			// remove all url from tweet if exists
+			$cleanTweet = $newTweet->removeUrlFromTweet($tweet['entities']['urls'], $tweet['text']);
+
+			// 2nd process
+			// remove common words and twitter slang words
+			$cleanTweet = $newTweet->removeCommonWords($cleanTweet);
+
+			// 3rd process
+			// remove special chars
+			$cleanTweet = $newTweet->clean($cleanTweet);
+
+			//echo 'Dirty tweet: '.$tweet['text'].'<br />';
+			//echo 'Clean tweet: '.$cleanTweet.'<br />';
+
+			$tokens = explode(" ", $cleanTweet);
+
+			foreach ($tokens as $term) {
+				if(($term != '') && ($term != '-')) {
+					$termFrequency = $termCollection->findOne(array('term' => $term));
+					if($termFrequency['frequency'] == NULL)	{
+						$termCollection->insert(array(
+							'term' => $term,
+							'frequency' => 1)
+						);
+					} else {
+						$nr_frequency = $termFrequency['frequency'] + 1;
+						$termCollection->update(
+							array('term' => $term),
+							array('$set' => array('frequency' => $nr_frequency))
+						);
+					}
+				}
+			}
+
+			/*
+			// if you want to use the first 100 rows
+			if($allTweets == 100) {
+				break;
+			}
+			*/
+		}
+
+	}
+} else {
+	echo '<a class="btn btn-success" href="index.php?route=term-frequency&func=run" role="button">Extract Term Frequency</a>';
+	echo '<hr />';
+}
+
 // count rows in the collection
 $allTweets = $collection->count();
 echo 'All tweets: <strong>'.$allTweets.'</strong><br />';
@@ -34,53 +90,9 @@ echo 'All tweets: <strong>'.$allTweets.'</strong><br />';
 $tweetsInEnglish = $collection->count(array('lang'=>'en'));
 echo 'Tweets in english: <strong>'.$tweetsInEnglish.'</strong><br />';
 
-// find everything in english from the collection
-$documents = $collection->find(array('lang'=>'en'));
-
-// iterate through the results
-foreach ($documents as $tweet) {
-	// 1st process
-	// remove all url from tweet if exists
-	$cleanTweet = $newTweet->removeUrlFromTweet($tweet['entities']['urls'], $tweet['text']);
-
-	// 2nd process
-	// remove common words and twitter slang words
-	$cleanTweet = $newTweet->removeCommonWords($cleanTweet);
-
-	// 3rd process
-	// remove special chars
-	$cleanTweet = $newTweet->clean($cleanTweet);
-
-	//echo 'Dirty tweet: '.$tweet['text'].'<br />';
-	//echo 'Clean tweet: '.$cleanTweet.'<br />';
-
-	$tokens = explode(" ", $cleanTweet);
-
-	foreach ($tokens as $term) {
-		if(($term != '') && ($term != '-')) {
-			$termFrequency = $termCollection->findOne(array('term' => $term));
-			if($termFrequency['frequency'] == NULL)	{
-				$termCollection->insert(array(
-					'term' => $term,
-					'frequency' => 1)
-				);
-			} else {
-				$nr_frequency = $termFrequency['frequency'] + 1;
-				$termCollection->update(
-					array('term' => $term),
-					array('$set' => array('frequency' => $nr_frequency))
-				);
-			}
-		}
-	}
-	
-	/*
-	// if you want to use the first 100 rows
-	if($allTweets == 100) {
-		break;
-	}
-	*/
-}
+// count english rows in the collection
+$terms = $termCollection->count();
+echo 'Terms: <strong>'.$terms.'</strong><br />';
 
 echo '<hr />';
 
@@ -88,7 +100,8 @@ echo '<hr />';
 $limitRow = 10;
 echo 'Show <strong>'.$limitRow.'</strong> most frequently terms used.<br /><br />';
 
-$terms = $termCollection->find()->limit(10);
+		
+$terms = $termCollection->find()->limit($limitRow);
 $terms = $terms->sort(array('frequency' => -1));
 echo '<div class="Table">';
 foreach ($terms as $term) {
